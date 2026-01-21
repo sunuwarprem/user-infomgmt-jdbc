62161e22f512c54144dfc02aba35d2a1d7686e7c
prem sunuwar <rhel8.prem@gmail.com>
2026-01-20 22:44:35 -0500

    removing comment

--- src/main/java/com/userinfo/jdbc/controller/UserController.java
+++ src/main/java/com/userinfo/jdbc/controller/UserController.java
@@ -44,7 +44,7 @@ public class UserController {
                    if(deleted>0){
                        System.out.println("Deleted from table");
                        return;
-                   }System.out.println("Error on delete from table"); //added commentgit
+                   }System.out.println("Error on delete from table"); //added
                    break;
                 case "list":
                     List<User> users=userService.listUser();
